fb8ea45626e3a467a2e6b16bddb1852b0f97c4e0
Markku Velinen <dzarg@lacus.(none)>
2010-02-15 20:28:03 +0200

    Forgot to actually add the files to the commit. So, another try.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,12 +1,24 @@
-#include <QtCore/QCoreApplication>
 #include <iostream>
+#include <QtCore/QCoreApplication>
 #include "client.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    std::cout << "Connecting!" << std::endl;
+
     Client client;
-    client.connectTo("dzarg.dy.fi", 6668);
+    std::cout << "Connecting" << std::endl;
+    if (client.connectTo("dzarg.dy.fi", 6668)) {
+        std::cout << "Connected!" << std::endl;
+
+        client.sendMessage("Hello World");
+        std::cout << "Text sent!" << std::endl;
+
+        client.sendMouseMovement(200, 200);
+        std::cout << "Mouse movement sent!" << std::endl;
+    }
+    else {
+        std::cout << "Connection failed :(" << std::endl;
+    }
     return a.exec();
 }
