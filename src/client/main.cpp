e3963caa25310cdf99b9277eed1e00a4e014c456
Niko Hämäläinen <milo@meelo.org>
2010-02-16 08:25:20 +0200

    Created in.txt which is a mock-up for the actual sensor file in n900. Also implemented file reading.. All in main, but will be refactored later to elsewhere.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 #include <QtCore/QCoreApplication>
+#include <QThread>
+#include <QByteArray>
+#include <QTextStream>
+#include <QFile>
 #include "client.h"
+
+class MyThread : public QThread
+{
+public:
+    static void msleep(unsigned long msecs)
+    {
+        QThread::msleep(msecs);
+    }
+};
+
+class SensorData
+{
+    QByteArray line;
+    QFile file;
+
+public:
+    SensorData(const QString& filename) : file(filename) {}
+    bool update()
+    {
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            return false;
+        }
+        line = file.readLine();
+        file.close();
+    }
+    QByteArray getLine() const { return line; }
+};
 
 int main(int argc, char *argv[])
 {
@@ -8,14 +39,19 @@ int main(int argc, char *argv[])
 
     Client client;
     std::cout << "Connecting" << std::endl;
-    if (client.connectTo("dzarg.dy.fi", 6668)) {
+    if (client.connectTo("lolcathost", 6668)) {
         std::cout << "Connected!" << std::endl;
 
         client.sendMessage("Hello World");
         std::cout << "Text sent!" << std::endl;
-
-        client.sendMouseMovement(200, 200);
-        std::cout << "Mouse movement sent!" << std::endl;
+        SensorData sensor("in.txt");
+        for (int i = 0; i < 5; ++i) {
+            sensor.update();
+            client.sendMessage(sensor.getLine());
+            MyThread::msleep(100);
+            client.sendMouseMovement(200, 200);
+            std::cout << "Mouse movement sent!" << std::endl;
+        }
 
         client.sendClick(3);
         std::cout << "Right mouse click sent!" << std::endl;
