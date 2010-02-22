6c203442d226c14e117bb21c6acdab7a340a626d
Niko Hämäläinen <milo@meelo.org>
2010-02-23 00:11:12 +0200

    there is no point locking other buttons with lock buttons, since the display only allows one button to be precced at once..

--- src/gui/mousewindow.cpp
+++ src/gui/mousewindow.cpp
@@ -21,26 +21,19 @@ MouseWindow::MouseWindow(ConnectionWindow* connectionWindow, QWidget *parent)
 
 void MouseWindow::processScrollBarActivity(int delta)
 {
-    if (!locked) {
-        qDebug() << delta;
-        engine->sendScroll(delta);
-    }
+    qDebug() << delta;
+    engine->sendScroll(delta);
 }
 
 void MouseWindow::leftMouseButtonClicked()
 {
-    if (!locked) {
-        qDebug() << "Left button clicked";
-        engine->sendClick(1);
-    }
+    qDebug() << "Left button clicked";
+    engine->sendClick(1);
 }
 
 void MouseWindow::rightMouseButtonClicked()
 {
-    if (!locked) {
-        qDebug() << "Right button clicked";
-        engine->sendClick(3);
-    }
+    qDebug() << "Right button clicked";
 }
 
 void MouseWindow::toggleButtonClicked()
